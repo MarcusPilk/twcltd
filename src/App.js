8500a59188cc438a58544c10e00829cfeadc123f
marcuspilkington <marcuspilkington@outlook.com>
2018-12-04 10:25:09 +0000

    Dev branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AnimatedEllipsis from 'react-animated-ellipsis';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h4> Website Coming Soon...</h4>
+          <h4> Website Coming Soon<AnimatedEllipsis/></h4>
           <img src={logo} className="App-logo" alt="logo" />
         </header>
       </div>
